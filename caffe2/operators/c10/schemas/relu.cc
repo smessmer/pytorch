aed9f89fc9487584a8df4baa79686f380ae6a85f
Sebastian Messmer <messmer@fb.com>
2018-06-26 16:03:34 -0700

    Add c10 relu op

---
+++ caffe2/operators/c10/schemas/relu.cc
@@ -0,0 +1,12 @@
+#include "caffe2/operators/c10/schemas/relu.h"
+#include "caffe2/core/operator_c10wrapper.h"
+#include "caffe2/core/dispatch/OpSchemaRegistration.h"
+
+using caffe2::Tensor;
+using caffe2::CPUContext;
+
+C10_DEFINE_OP_SCHEMA(caffe2::ops::Relu);
+
+namespace caffe2 {
+    REGISTER_C10_OPERATOR_FOR_CAFFE2_DISPATCH(ops::Relu, C10Relu_DontUseThisOpYet)
+}
