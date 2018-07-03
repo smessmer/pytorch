b27aa3fb48b1d96c1062a5572591f9ab9611843c
Sebastian Messmer <messmer@fb.com>
2018-07-03 16:01:37 -0700

    Implement c10 SigmoidCrossEntropyWithLogits op

---
+++ caffe2/operators/c10/schemas/sigmoid_cross_entropy_with_logits.cc
@@ -0,0 +1,25 @@
+#include "caffe2/operators/c10/schemas/sigmoid_cross_entropy_with_logits.h"
+#include "caffe2/core/operator_c10wrapper.h"
+#include "caffe2/core/dispatch/OpSchemaRegistration.h"
+
+using caffe2::Tensor;
+using caffe2::CPUContext;
+
+C10_DEFINE_OP_SCHEMA(caffe2::ops::SigmoidCrossEntropyWithLogits);
+
+namespace {
+struct LogDTrickParameter final {
+    using type = bool;
+    static constexpr const char* name() { return "log_D_trick"; }
+    static constexpr bool default_value() { return false; }
+};
+struct UnjoinedLRLossParameter final {
+    using type = bool;
+    static constexpr const char* name() { return "unjoined_lr_loss"; }
+    static constexpr bool default_value() { return false; }
+};
+}
+
+namespace caffe2 {
+    REGISTER_C10_OPERATOR_FOR_CAFFE2_DISPATCH_WITH_PARAMETERS(ops::SigmoidCrossEntropyWithLogits, void, C10SigmoidCrossEntropyWithLogits_DontUseThisOpYet, ParameterHelper<LogDTrickParameter>, ParameterHelper<UnjoinedLRLossParameter>)
+}
